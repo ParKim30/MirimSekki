84f710c99a8be905acfe5ce4e301818102ad2ce8
Kimyen0426 <s2018s01@e-mirim.hs.kr>
2019-09-30 19:08:58 +0900

    알레르기 정보 db에 추가 문법 수정

--- signup.php
+++ signup.php
@@ -8,13 +8,31 @@
 
     $uid = $_POST['uid'];
     $upw = $_POST['upw'];
+    $uname = $_POST['name'];
     $grade = $_POST['grade'];
     $classroom = $_POST['classroom'];
     $num = $_POST['num'];
     $home = $_POST['home'];
+    $allergy = $_POST['allergy'];
 
-    $sql = "insert into member (id, pw, grade, classroom, num, dor)";
-    $sql = $sql. "values('{$uid}','{$upw}','{$grade}','{$classroom}','{$num}','{$home}')";
+    // for($i=0;$i < count($allergy);$i++) {
+    //     echo $allergy[$i] . "<br>";
+    //   }
+   
+    if(count($allergy)==0){
+        $exist = false;
+    }else if(count($allergy)>0){
+        $exist = true;
+        $sql2 = "insert into member_allergy(id,allergy_1,allergy_2,allergy_3,allergy_4,allergy_5,allergy_6,allergy_7,allergy_8,allergy_9,allergy_10,allergy_12,allergy_13,allergy_14,allergy_15,allergy_16,allergy_17,allergy_18)";
+    }
+
+    $sql = "insert into member (id, pw, name,grade, classroom, num, dor,allergy)";
+    $sql = $sql. "values('{$uid}','{$upw}','{$uname}','{$grade}','{$classroom}','{$num}','{$home}','{$exist}')";
+
+     //이제 알레르기여부 보고 있으면 프라이머리키 가져와서 넣어주고 알레르기여부도 넣어줘야 함
+    if($exist==true){
+        $sql2 = $sql2. "values('{$uid}','{$allergy}')";
+    }
 
     if($conn->query($sql)){
         echo("<script>alert('회원가입 되었습니다')</script>");
